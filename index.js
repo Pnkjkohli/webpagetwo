4cfb81553f4bf8328b845abf1689b4a8bea030b2
Priyanka <pankaj.kohli@poplify.com>
2025-01-28 18:56:58 +0530

    button

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 // see all button for third section
-function addField() {
-  var newcontents = document.getElementById("see_all_contents");
-  var contents = document.getElementById("contents");
-  newcontents.appendChild(contents.cloneNode(true));
-}
+document.getElementById("seeAllBtn").addEventListener("click", function() {
+  // var newcontents = document.getElementById("see_all_contents");
+  // var contents = document.getElementById("contents");
+  // newcontents.appendChild(contents.cloneNode(true));
+  document.getElementById("see_all_contents").style.display = "block";
+// if(see_all_contents.style.display === "none"){
+//   this.innerHTML="See All";
+//   this.id="seeAllBtn";
+// }
+
+// else(see_all_contents.style.display === "block")
+// {
+
+//   this.innerHTML="View Less";
+//   this.id="viewLessBtn";
+//   document.getElementById("see_all_contents").style.display = "block";
+// }
+
+  this.innerHTML="View Less";
+  this.id="viewLessBtn";
+  document.getElementById("viewLessBtn").addEventListener("click", function() {
+    document.getElementById("see_all_contents").style.display = "none";
+    this.innerHTML="See All";
+      this.id="seeAllBtn";
+
+  });
+});
+
+
+
 // header  menu for mobile
 function menuFunction() {
   document.getElementById("onlymobile").style.display = "none";
@@ -15,6 +40,7 @@ function menuFunction() {
 
 // hide mobile menu 
 document.getElementById("crossicon").addEventListener("click", function() {
+
   document.getElementById("onlymobile").style.display = "block";
   document.getElementById("crossicon").style.display = "none";
   document.getElementById("headerText").style.display = "block";
